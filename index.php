e74cbbac8ea8cd3715e0018fa925647a52aa7349
sav <aevs.andres.vidal@gmail.com>
2024-11-25 18:01:38 -0600

    environment variables config has been added

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
-require 'controller/Controller.php';
+require_once 'vendor/autoload.php';
+require_once 'controller/Controller.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 Controller::main();
