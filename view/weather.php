479506240da982fada49243087bba398601eec6f
sav <aevs.andres.vidal@gmail.com>
2024-11-27 00:08:16 -0600

    now, the information is being fetched from the OpenWeather API

--- view/weather.php
+++ view/weather.php
@@ -4,46 +4,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple weather</title>
+    <meta name="description" content="Weather app displaying current weather, visibility, humidity, and forecast for multiple days">
+    <meta name="keywords" content="weather, forecast, temperature, humidity, wind">
     <link href="public/css/styles.css" rel="stylesheet">
     <script src="https://unpkg.com/feather-icons"></script>
+    <title>Simple weather</title>
 </head>
 
 <body>
+    <?php
+    require_once 'utils/Helper.php';
+    $helper = new Helper($variables);
+    ?>
     <div class="container">
         <div class="weather-side">
             <div class="weather-gradient"></div>
             <div class="date-container">
-                <h2 class="date-dayname">Tuesday</h2>
-                <span class="date-day">10 May 2024</span>
+                <h2 class="date-dayname"><?php echo ($helper->getValue(0, 'day')) ?></h2>
+                <span class="date-day"><?php echo ($helper->getValue(0, 'date')) ?></span>
                 <br>
                 <div class="location">
                     <i class="location-icon" data-feather="map-pin"></i>
-                    <span>Morelos, MX</span>
+                    <span><?php echo ($helper->getValue(0, 'location')) ?></span>
                 </div>
             </div>
             <div class="weather-container">
-                <i class="weather-icon" data-feather="sun"></i>
-                <h1 class="weather-temp">29°C</h1>
-                <h3 class="weather-desc">Sunny</h3>
+                <img src="<?php echo ($helper->getValue(0, 'icon')) ?>" alt="Icon Wather">
+                <h1 class="weather-temp"><?php echo ($helper->getValue(0, 'temperature')) ?>°C</h1>
+                <h3 class="weather-desc"><?php echo ($helper->getValue(0, 'description')) ?></h3>
             </div>
         </div>
         <div class="info-side">
             <div class="today-info-container">
                 <div class="today-info">
                     <div class="precipitation">
-                        <span class="title">PRECIPITATION</span>
-                        <span class="value">0 %</span>
+                        <span class="title">VISIBILITY</span>
+                        <span class="value"><?php echo ($helper->getValue(0, 'visibility')) ?> km</span>
                         <div class="clear"></div>
                     </div>
                     <div class="humidity">
                         <span class="title">HUMIDITY</span>
-                        <span class="value">34 %</span>
+                        <span class="value"><?php echo ($helper->getValue(0, 'humidity')) ?> %</span>
                         <div class="clear"></div>
                     </div>
                     <div class="wind">
                         <span class="title">WIND</span>
-                        <span class="value">0 km/h</span>
+                        <span class="value"><?php echo ($helper->getValue(0, 'wind')) ?> km/h</span>
                         <div class="clear"></div>
                     </div>
                 </div>
@@ -51,24 +57,24 @@
             <div class="week-container">
                 <ul class="week-list">
                     <li class="active">
-                        <i class="day-icon" data-feather="sun"></i>
-                        <span class="day-name">Tue</span>
-                        <span class="day-temp">29°C</span>
+                        <img src="<?php echo ($helper->getValue(1, 'icon')) ?>" alt="Icon Wather">
+                        <span class="day-name"><?php echo ($helper->getValue(1, 'day')) ?></span>
+                        <span class="day-temp"><?php echo ($helper->getValue(1, 'temperature')) ?>°C</span>
                     </li>
                     <li>
-                        <i class="day-icon" data-feather="cloud"></i>
-                        <span class="day-name">Wed</span>
-                        <span class="day-temp">21°C</span>
+                        <img src="<?php echo ($helper->getValue(2, 'icon')) ?>" alt="Icon Wather">
+                        <span class="day-name"><?php echo ($helper->getValue(2, 'day')) ?></span>
+                        <span class="day-temp"><?php echo ($helper->getValue(2, 'temperature')) ?>°C</span>
                     </li>
                     <li>
-                        <i class="day-icon" data-feather="cloud-snow"></i>
-                        <span class="day-name">Thu</span>
-                        <span class="day-temp">08°C</span>
+                        <img src="<?php echo ($helper->getValue(3, 'icon')) ?>" alt="Icon Wather">
+                        <span class="day-name"><?php echo ($helper->getValue(3, 'day')) ?></span>
+                        <span class="day-temp"><?php echo ($helper->getValue(3, 'temperature')) ?>°C</span>
                     </li>
                     <li>
-                        <i class="day-icon" data-feather="cloud-rain"></i>
-                        <span class="day-name">Fry</span>
-                        <span class="day-temp">19°C</span>
+                        <img src="<?php echo ($helper->getValue(4, 'icon')) ?>" alt="Icon Wather">
+                        <span class="day-name"><?php echo ($helper->getValue(4, 'day')) ?></span>
+                        <span class="day-temp"><?php echo ($helper->getValue(4, 'temperature')) ?>°C</span>
                     </li>
                     <div class="clear"></div>
                 </ul>
@@ -80,6 +86,7 @@
                 </button>
             </div>
         </div>
+        <div style="position: fixed; bottom: 10%; left: 42.5%; font-weight: 600; color: #222831;">Developed by Samoel Andres</div>
     </div>
     <script src="public/script/script.js"></script>
 </body>
